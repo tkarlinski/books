99dc7b72fcfc6af0ba2c7dd8e75f1fee6d157975
tkarlinski <karlinski.tomasz@gmail.com>
2019-05-03 20:47:18 +0200

    require symfony/twig-bundle

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
     /**
+     * @Template()
      * @Route("/index", name="index")
      */
-    public function index()
+    public function index() : array
     {
+        return [];
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/IndexController.php',
