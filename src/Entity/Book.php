713a715335629e4edbd0bc00892a3b0817901e66
tkarlinski <karlinski.tomasz@gmail.com>
2019-06-28 23:22:45 +0200

    entity book

---
+++ src/Entity/Book.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BookRepository")
+ */
+class Book
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="smallint", nullable=true)
+     */
+    private $publishYear;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $pages;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $isbn;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPublishYear(): ?int
+    {
+        return $this->publishYear;
+    }
+
+    public function setPublishYear(?int $publishYear): self
+    {
+        $this->publishYear = $publishYear;
+
+        return $this;
+    }
+
+    public function getPages(): ?int
+    {
+        return $this->pages;
+    }
+
+    public function setPages(int $pages): self
+    {
+        $this->pages = $pages;
+
+        return $this;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(string $isbn): self
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+}
